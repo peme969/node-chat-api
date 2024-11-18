f207e5a818dd9dd2dc65745749e84a9f53d8d174
Peme969 ⚡ <136040410+peme969@users.noreply.github.com>
2024-11-18 11:37:30 -0600

    Update gpt4js.js

--- api/utils/gpt4js.js
+++ api/utils/gpt4js.js
@@ -2,8 +2,9 @@ import getGPT4js from 'gpt4js';
 
 let GPT4js;
 
-(async () => {
-  GPT4js = await getGPT4js();
-})();
-
-export default GPT4js;
+export const initializeGPT4js = async () => {
+  if (!GPT4js) {
+    GPT4js = await getGPT4js();
+  }
+  return GPT4js;
+};
