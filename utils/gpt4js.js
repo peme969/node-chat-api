9e6390a5166b4204d6fd72aacdef1a65a39fd59f
Peme969 ⚡ <136040410+peme969@users.noreply.github.com>
2024-11-18 11:33:57 -0600

    Delete utils directory

--- utils/gpt4js.js
+++
@@ -1,9 +0,0 @@
-import getGPT4js from 'gpt4js';
-
-let GPT4js;
-
-(async () => {
-  GPT4js = await getGPT4js();
-})();
-
-export default GPT4js;
--- utils/validateRequest.js
+++
@@ -1,6 +0,0 @@
-export default function validateRequest(params) {
-  const { messages, prompt, provider, model } = params;
-  if (!provider) throw new Error('Provider is required.');
-  if (messages && !Array.isArray(messages)) throw new Error('Messages should be an array.');
-  if (prompt && typeof prompt !== 'string') throw new Error('Prompt should be a string.');
-}
